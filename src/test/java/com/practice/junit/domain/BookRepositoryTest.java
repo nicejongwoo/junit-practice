52f5294bcfc90e3170d25552327d8e686b68175f
leejongwoo <leejongwoo.dev@gmail.com>
2022-08-29 21:26:56 +0900

    Register IoC, Test method for repository

--- src/test/java/com/practice/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/practice/junit/domain/BookRepositoryTest.java
@@ -1,7 +1,20 @@
 package com.practice.junit.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @DisplayName("책등록테스트")
+    @Test
+    void save_test() {
+        System.out.println("책등록 테스트");
+    }
 
 }
