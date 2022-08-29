7bef21361b0e1e917584c30185f0045d28872002
leejongwoo <leejongwoo.dev@gmail.com>
2022-08-29 21:13:31 +0900

    Create Book Entity

--- src/main/java/com/practice/junit/domain/Book.java
+++ src/main/java/com/practice/junit/domain/Book.java
@@ -1,4 +1,26 @@
 package com.practice.junit.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
 public class Book {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String title;
+    private String author;
+
 }
