e5739349dba8246b19fc8e095d543471428e026f
leejongwoo <leejongwoo.dev@gmail.com>
2022-08-30 12:16:43 +0900

    Test BookRepository save()

--- src/test/java/com/practice/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/practice/junit/domain/BookRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.practice.junit.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 class BookRepositoryTest {
@@ -14,7 +17,22 @@ class BookRepositoryTest {
     @DisplayName("책등록테스트")
     @Test
     void save_test() {
-        System.out.println("책등록 테스트");
+        //given
+        String title = "Spring Boot";
+        String author = "user";
+
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        //when
+        Book savedBook = bookRepository.save(book);
+
+        //then
+        assertThat(savedBook).isNotNull();
+        assertThat(savedBook.getId()).isGreaterThan(0);
+        assertThat(savedBook.getTitle()).isEqualTo(book.getTitle());
     }
 
 }
