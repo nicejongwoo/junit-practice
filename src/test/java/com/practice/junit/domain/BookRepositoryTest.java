0ba940bfcdd5a2650aeb8be7334eff483f12925b
leejongwoo <leejongwoo.dev@gmail.com>
2022-08-30 16:41:08 +0900

    Test findAllBooks and findById

--- src/test/java/com/practice/junit/domain/BookRepositoryTest.java
+++ src/test/java/com/practice/junit/domain/BookRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.practice.junit.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,9 +17,21 @@ class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+    @BeforeEach
+    void readyData() {
+        String title = "Spring Boot";
+        String author = "user";
+
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    }
+
     @DisplayName("책등록테스트")
     @Test
-    void save_test() {
+    void saveTest() {
         //given
         String title = "Spring Boot";
         String author = "user";
@@ -33,6 +48,31 @@ class BookRepositoryTest {
         assertThat(savedBook).isNotNull();
         assertThat(savedBook.getId()).isGreaterThan(0);
         assertThat(savedBook.getTitle()).isEqualTo(book.getTitle());
+    }
+
+    @DisplayName("책목록 조회 테스트")
+    @Test
+    void findAllTest() {
+        //given
+
+        //when
+        List<Book> all = bookRepository.findAll();
+
+        //then
+        assertThat(all.size()).isGreaterThan(0);
+        assertThat(all.size()).isLessThan(2);
+    }
+
+    @DisplayName("책 한권 조회 테스트")
+    @Test
+    void findByIdTest() {
+        //given
+        //when
+        Book book = bookRepository.findById(1L).get();
+
+        //then
+        assertThat(book.getTitle()).isEqualTo("Spring Boot");
+        assertThat(book.getAuthor()).isEqualTo("user");
     }
 
 }
